d8a8efbb771be851140cc0d1e086f9dfe2432ed0
Doshuaaa <jjeun9910@gmail.com>
2024-04-16 23:47:42 +0900

    first init

--- RecommendTravelDestinations/app/src/main/java/com/doshua/recommendtraveldestinations/MainActivity.kt
+++ RecommendTravelDestinations/app/src/main/java/com/doshua/recommendtraveldestinations/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.doshua.recommendtraveldestinations
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import com.doshua.recommendtraveldestinations.R
+import androidx.databinding.DataBindingUtil
+import com.doshua.recommendtraveldestinations.databinding.ActivityMainBinding
+import com.doshua.recommendtraveldestinations.view_model.MapViewModel
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -16,39 +23,85 @@ import com.google.android.gms.maps.model.MarkerOptions
 class MaiActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var gMap: GoogleMap
+    private lateinit var locationManager: LocationManager
+    private lateinit var binding: ActivityMainBinding
+    private val  mapFragment by lazy { supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment }
+    private val mapViewModel: MapViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+        binding.apply {
+            viewModel = mapViewModel
+            lifecycleOwner = this@MaiActivity
+        }
+
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
     }
 
+    override fun onResume() {
+        super.onResume()
+        checkPermission()
+    }
     override fun onMapReady(googleMap: GoogleMap) {
 
         gMap = googleMap
-        val seoul = LatLng(37.556, 126.97)
-        val markerOptions = MarkerOptions()
 
-        markerOptions.apply {
-            title("서울")
-            position(seoul)
-            snippet("한국 수도")
-        }
+        val fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        try {
+            fusedLocationClient.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, null).addOnSuccessListener {
+                if(it != null) {
+                    val currLocation = it
 
-        gMap.addMarker(markerOptions)
+                    var currPosition: LatLng? = null
+                    currPosition = LatLng(currLocation.latitude, currLocation.longitude)
 
-        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(seoul, 15f))
+                    val markerOptions = MarkerOptions()
+
+                    markerOptions.apply {
+                        title("서울")
+                        position(currPosition)
+                        snippet("한국 수도")
+                    }
+
+                    gMap.addMarker(markerOptions)
+
+                    gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currPosition, 15f))
+                }
+            }
+        } catch (e: SecurityException) { e.printStackTrace() }
     }
 
-    fun checkPermission() {
+    private fun checkPermission() {
 
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
             == PackageManager.PERMISSION_DENIED
         ) {
+            mapViewModel.setPermissionDenied(true)
 
-            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
+            if(mapViewModel.firstRequestFlag.value!!) {
+                mapViewModel.setFirstRequestFlag(false)
+                requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
+            }
+        } else {
+            mapFragment.getMapAsync(this)
+            mapViewModel.setPermissionDenied(false)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            mapViewModel.setPermissionDenied(false)
+        } else {
+            mapViewModel.setPermissionDenied(true)
         }
     }
 }
--- RecommendTravelDestinations/app/src/main/java/com/doshua/recommendtraveldestinations/view_model/MapViewModel.kt
+++ RecommendTravelDestinations/app/src/main/java/com/doshua/recommendtraveldestinations/view_model/MapViewModel.kt
@@ -1,2 +1,26 @@
-package com.doshua.recommendtraveldestinations.view_model 
+package com.doshua.recommendtraveldestinations.view_model
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MapViewModel : ViewModel() {
+
+    private val _permissionDenied = MutableLiveData<Boolean>()
+    val permissionDenied get() = _permissionDenied
+
+    private val _firstRequestFlag = MutableLiveData<Boolean>()
+    val firstRequestFlag get() = _firstRequestFlag
+
+    init {
+        _firstRequestFlag.value = true
+    }
+
+    fun setPermissionDenied(isDenied: Boolean) {
+
+        _permissionDenied.value = isDenied
+    }
+
+    fun setFirstRequestFlag(flag: Boolean) {
+        _firstRequestFlag.value = flag
+    }
+}
